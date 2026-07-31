19ff092df5b41c31f27f213aa63bc32d2b5653d3
amogh16102013 <amogh.nalli@gmail.com>
2026-07-31 17:59:43 -0400

    file and error learning

---
+++ errors.py
@@ -0,0 +1,45 @@
+# num = int(input("choose a number"))
+# print(1/num)
+
+# try:
+#     num = int(input("choose a number"))
+#     print(1/num)
+# except ZeroDivisionError:
+#     print("choose non zero no.")
+# except ValueError: 
+#     print("use a whole number")
+
+
+# try:
+#     score = 1
+#     print(score)
+# except NameError:
+#     print("not assigned var")
+
+
+# try:
+#     num = 1
+#     name = "i"
+#     print(num+name)
+# except TypeError:
+#     print("cannot add str and int")
+
+
+# try:
+#     dct = {"num": 1}
+#     print(dct["name"])
+# except KeyError:
+#     print("not existing key")
+
+# try:
+#     lst = [1, 2, 3, 4]
+#     print(lst[4])
+# except IndexError:
+#     print("not in list")
+
+# try:
+#     val = 0
+#     val.append(3)
+# except AttributeError:
+#     print("not a list")
+
---
+++ file.py
@@ -0,0 +1,18 @@
+# file = open("stuff.txt", "r")
+# print(file.read())
+
+# file = open("stuff.txt", "r")
+# file.readline()
+# print(file.readline())
+
+# file = open("stuff.txt", "r")
+# lst = file.readlines()
+# for i in lst:
+#     print(i)
+
+file = open("stuff.txt", "r")
+file.readline()
+file.readline()
+print(file.readline())
+file.close()
+
